533e914c2a6ee8d4d139a9836a5d60dce46ce557
Festus Kogo <fkogoson@gmail.com>
2023-03-27 11:58:30 +0300

    Fix password_generattor

--- password_generator.py
+++ password_generator.py
@@ -1,39 +1,22 @@
-import random
-import string
+import random, string
 
-s_letters = list(string.ascii_lowercase)
-s_letters.remove('l') # Exclude small letter l
-s_letters.remove('o') # Exclude small letter o
+lower = list(string.ascii_lowercase)
+lower.remove('l') # Exclude l
+lower.remove('o') # Exclude o
+lower_case = "".join(lower)
 
-# choose 4 random small letter characters using list comprehension
-random_small_ch = [random.choice(s_letters) for i in range(4)]  
-# print(random_small_ch) # ['d', 'c', 'm', 'm']
+upper = list(string.ascii_uppercase)
+upper.remove('O') # Exclude O
+upper.remove('I') # Exclude I
+upper_case = "".join(upper)
 
-b_letters = list(string.ascii_uppercase)
-b_letters.remove('O') # Exclude capital letter O
-b_letters.remove('I') # Exclude capital letter I
+numbers = "23456789" # Exclude 0 and 1
 
-random_cap_ch = [random.choice(b_letters) for i in range(4)]
-# print(random_cap_ch) # ['B', 'H', 'V', 'I']
+special_chars = "@#$&_-()=%*:/!?+."
 
-numbers = list(range(2, 10)) # exclude numbers 0 and 1 to avoid confusing with letters l and o or O respectively
-random_nums = [random.choice(numbers) for i in range(2)]
-# print(random_nums) # [9, 5]
+pass_raw = lower_case + upper_case + numbers + special_chars
 
-special_chars = ['!', '@', '#', '$', '%', '&', '*']
+length = int(input("How Many Characters Do You Want Your Password To Be: "))
+password = "".join(random.sample(pass_raw, length))
 
-special_chars = [random.choice((special_chars)) for i in range(2)]
-# print(special_chars) # ['*', '@']
-
-# combine all characters
-raw_chars = random_small_ch + random_cap_ch + random_nums + special_chars
-
-shuffled_chars = [random.choice(raw_chars) for i in range(len(raw_chars))]
-
-def converts(s):
-    new = ""
-    for c in s:
-        new += str(c)
-    return new
-
-print(converts(shuffled_chars))
+print("Password: " + password)
